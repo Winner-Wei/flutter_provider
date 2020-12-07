35b6148166f5d7c8312c6cce90cec62fb8cbd93b
weiwei <17601068937@163.com>
2020-12-07 17:50:42 +0800

    优化代码

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,37 @@ import 'package:provider/provider.dart';
 import './pages/changeTheme.dart';
 import './model/ThemeModal.dart';
 
+
+// Provider
+// ChangeNotiferProvider
+// ListenableProvider
+// ValueListenableProvider
+// StreamProvider
+// FutureProvider
+
+// Provider.of
+// Consumer
+// Selector
+
+// 更新数据的方式 ChangeNotifer 
+
 void main() => runApp(
-    ChangeNotifierProvider<ThemeModal>(
-        create: (_) {
-            return ThemeModal(ThemeType.light);
-        },
-        child: MyApp()
+    ValueListenableProvider<int>(
+        create: (_) => ValueNotifier(0),
+        child: ChangeNotifierProvider<ThemeModal>(
+            create: (_) {
+                return ThemeModal(ThemeType.light);
+            },
+            child: MyApp()
+        )
     )
 );
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
-        theme: Provider.of<ThemeModal>(context).themeData,
+        theme: Provider.of<ThemeModal>(context).themeData, // provider 获取数据01
         home: MyHomePage(title: 'Flutter Provider'));
   }
 }
--- lib/pages/changeTheme.dart
+++ lib/pages/changeTheme.dart
@@ -13,69 +13,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text('哈哈哈'),
-                        SizedBox(height: 10),
-                        Text('You have pushed the button this many times:'),
-                        SizedBox(height: 10),
-                        Selector(
-                            builder: (context, data, child) {
-                                return Text('$data');
-                            }, 
-                            selector: (BuildContext context, ThemeModal themeModal) {
-                                return themeModal.countValue;
-                            },
-                            shouldRebuild: (pre, next) {
-                                return pre != next;
-                            },
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                                OutlineButton(
-                                    child: Text("加"),
-                                    onPressed: () {
-                                        Provider.of<ThemeModal>(context, listen: false).addCount();
-                                    }
-                                ),
-                                SizedBox(width: 10),
-                                OutlineButton(
-                                    child: Text("减"),
-                                    onPressed: () {
-                                        Provider.of<ThemeModal>(context, listen: false).reduceCount();
-                                    }
-                                )
-                            ]
-                        ),
-                        FlatButton(
-                            onPressed: () {
-                                Provider.of<ThemeModal>(context, listen: false).reverse();
-                            }, 
-                            child: Consumer<ThemeModal>(
-                                builder: (context, t, child) {
-                                    String result = '切换成';
-                                    if (t.currentType == ThemeType.dark) {
-                                        result += '白天模式'; 
-                                    } else {
-                                        result += '夜间模式';
-                                    }
-                                    return Text("$result", style: TextStyle(color: Colors.red)
-                                );
-                            }
-                        )
-                    )
-                ]
-            ))
-        );
-    }
+//   void constructor() {
+//     print('constructor');
+//   }
+
+  void initState() {
+    print('initState');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Text('哈哈哈'),
+              SizedBox(height: 10),
+              Text('You have pushed the button this many times:'),
+              SizedBox(height: 10),
+              Selector(
+                // provider 获取数据02
+                builder: (context, data, child) {
+                  return Text('$data');
+                },
+                selector: (BuildContext context, ThemeModal themeModal) {
+                  return themeModal.countValue;
+                },
+                shouldRebuild: (pre, next) {
+                  return pre != next;
+                },
+              ),
+              SizedBox(height: 10),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                OutlineButton(
+                    child: Text("加"),
+                    onPressed: () {
+                      Provider.of<ThemeModal>(context, listen: false)
+                          .addCount();
+                    }),
+                SizedBox(width: 10),
+                OutlineButton(
+                    child: Text("减"),
+                    onPressed: () {
+                      Provider.of<ThemeModal>(context, listen: false)
+                          .reduceCount();
+                    })
+              ]),
+              FlatButton(onPressed: () {
+                Provider.of<ThemeModal>(context, listen: false).reverse();
+              }, child: Consumer<ThemeModal>(// provider 获取数据03
+                  builder: (BuildContext context, t, child) {
+                String result = '切换成';
+                if (t.currentType == ThemeType.dark) {
+                  result += '白天模式';
+                } else {
+                  result += '夜间模式';
+                }
+                return Text("$result", style: TextStyle(color: Colors.red));
+              }))
+            ])));
+  }
 }
